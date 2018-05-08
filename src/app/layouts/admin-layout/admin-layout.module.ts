f061fdb9f15ce6d1adfa26eb880953464fbf5ce8
Adamos Kyriakou <somada141@gmail.com>
2018-05-08 10:54:04 +1000

    Removed `icons` component.

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,7 +6,6 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { IconsComponent } from '../../icons/icons.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 
 import {
@@ -29,7 +28,6 @@ import {
     DashboardComponent,
     UserProfileComponent,
     TableListComponent,
-    IconsComponent,
     NotificationsComponent,
   ]
 })
