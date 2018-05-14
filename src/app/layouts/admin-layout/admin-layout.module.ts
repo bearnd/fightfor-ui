82d45989ee28e4b76806ebcdf279c102b7e78ed6
Adamos Kyriakou <somada141@gmail.com>
2018-05-14 16:52:54 +0700

    Cleanup.

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AdminLayoutRoutes } from './admin-layout.routing';
-import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { SearchesComponent } from '../../searches/searches.component';
-
 import {
   MatButtonModule,
   MatInputModule,
@@ -16,6 +10,11 @@ import {
   MatChipsModule,
 } from '@angular/material';
 
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { SearchesComponent } from '../../searches/searches.component';
 import { SearchResultsComponent } from '../../searches/search-results/search-results.component';
 import { SearchNewComponent } from '../../searches/search-new/search-new.component';
 import { SearchesGridComponent } from '../../searches/searches-grid/searches-grid.component';
