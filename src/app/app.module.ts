007bde8c16cb339920a25a8a6905b4a8dd84431f
Adamos Kyriakou <somada141@gmail.com>
2018-05-26 10:50:26 +1000

    `app.module.ts`: Removed the `AgmModule` as it is not being used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AgmCoreModule } from '@agm/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
@@ -17,8 +16,8 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-
 import { SearchesService } from './searches/searches.service';
+import { TrialsManagerService } from './searches/trials-manager.service';
 
 import { environment } from '../environments/environment';
 
@@ -31,9 +30,6 @@ import { environment } from '../environments/environment';
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
-    }),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     SlimLoadingBarModule.forRoot(),
@@ -45,7 +41,11 @@ import { environment } from '../environments/environment';
     AppComponent,
     AdminLayoutComponent,
   ],
-  providers: [SearchesService, McBreadcrumbsService],
+  providers: [
+    SearchesService,
+    McBreadcrumbsService,
+    TrialsManagerService,
+  ],
   bootstrap: [AppComponent],
   exports: [
     McBreadcrumbsModule,
