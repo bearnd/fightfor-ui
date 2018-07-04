b900a47b0c6457bf67f1b1dc053da8f0f57f0b65
Adamos Kyriakou <somada141@gmail.com>
2018-07-04 21:31:00 +1000

    `mesh-descriptor-retriever.service.ts`: Added the functionality to fuzzy-search for MeSH descriptors by synonym (this commit should’ve happened in the previous branch).

--- src/app/services/mesh-descriptor-retriever.service.ts
+++ src/app/services/mesh-descriptor-retriever.service.ts
@@ -4,16 +4,30 @@ import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 
 import { MeshDescriptorInterface } from '../interfaces/mesh-descriptor.interface';
+import { Observable } from 'rxjs/Observable';
 
 
 interface ResponseGetMeshDescriptorsByTreeNumberPrefix {
   descriptors: {
-    byTreeNumberPrefix: MeshDescriptorInterface[];
+    byTreeNumberPrefix: MeshDescriptorInterface[]
   }
 }
 
 interface VariablesGetMeshDescriptorsByTreeNumberPrefix {
   treeNumberPrefix: string
+}
+
+// Response interface for the `getMeshDescriptorsBySynonym` method.
+interface ResponseGetMeshDescriptorsBySynonym {
+  descriptors: {
+    bySynonym: MeshDescriptorInterface[]
+  }
+}
+
+// Variables interface for the `getMeshDescriptorsBySynonym` method.
+interface VariablesGetMeshDescriptorsBySynonym {
+  synonym: string
+  limit: number
 }
 
 
@@ -32,14 +46,25 @@ export class MeshDescriptorRetrieverService {
     }
   `;
 
+  // GraphQL query used in the `getMeshDescriptorsBySynonym` method.
+  queryGetMeshDescriptorsBySynonym = gql`
+    query getMeshDescriptorsBySynonym($synonym: String!, $limit: Int!){
+      descriptors {
+        bySynonym(synonym: $synonym, limit: $limit) {
+          descriptorId,
+          name,
+          ui
+        }
+      }
+    }
+  `;
+
   constructor(private apollo: Apollo) {
   }
 
   getMeshDescriptorsByTreeNumberPrefix(treeNumberPrefix: string) {
-    return this.apollo.query<
-      ResponseGetMeshDescriptorsByTreeNumberPrefix,
-      VariablesGetMeshDescriptorsByTreeNumberPrefix
-    >({
+    return this.apollo.query<ResponseGetMeshDescriptorsByTreeNumberPrefix,
+      VariablesGetMeshDescriptorsByTreeNumberPrefix>({
       query: this.queryGetMeshDescriptorsByTreeNumberPrefix,
       variables: {
         treeNumberPrefix: treeNumberPrefix
@@ -48,4 +73,24 @@ export class MeshDescriptorRetrieverService {
       return response.data.descriptors.byTreeNumberPrefix;
     });
   }
+
+  /**
+   * Searches for MeSH descriptors by through synonym fuzzy-matching and returns a list of descriptors in order of descending relevance.
+   * @param {string} synonym The synonym query to be used in the fuzzy-search.
+   * @param {number} limit The maximum number of descriptors to be returned.
+   * @returns {Observable<MeshDescriptorInterface[]>} The matching descriptors in order of descending relevance.
+   */
+  getMeshDescriptorsBySynonym(synonym: string, limit: number): Observable<MeshDescriptorInterface[]> {
+    return this.apollo.query<ResponseGetMeshDescriptorsBySynonym,
+      VariablesGetMeshDescriptorsBySynonym>({
+      query: this.queryGetMeshDescriptorsBySynonym,
+      variables: {
+        synonym: synonym,
+        limit: limit,
+      }
+    }).map((response) => {
+      return response.data.descriptors.bySynonym;
+    });
+  }
+
 }
