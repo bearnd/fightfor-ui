12056d262110bec06a5ada98eed5d7b7e3573be4
Adamos Kyriakou <somada141@gmail.com>
2018-08-04 18:05:52 +1000

    Forgot this one.

--- src/app/interfaces/search.interface.ts
+++ src/app/interfaces/search.interface.ts
@@ -24,6 +24,7 @@ export interface SearchInterface {
   title?: string
   descriptors: MeshDescriptorInterface[]
   studies?: StudyInterface[]
+  studiesFiltered?: StudyInterface[]
   studiesStats: {
     byCountry?: CountByCountryInterface[]
     byOverallStatus?: CountByOverallStatusInterface[]
