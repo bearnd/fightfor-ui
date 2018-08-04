e4adc67752cd45bf8f96b7d4798062aafaa78235
Adamos Kyriakou <somada141@gmail.com>
2018-08-04 17:51:35 +1000

    Updated the GraphQL query methods to emit booleans through subjects to indicate when the values are being loaded.

--- src/app/services/study-retriever.service.ts
+++ src/app/services/study-retriever.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { StudyInterface } from '../interfaces/study.interface';
 import { MeshDescriptorInterface } from '../interfaces/mesh-descriptor.interface';
 
 
 interface VariablesSearchStudies {
-  meshDescriptorIds: Number[]
+  meshDescriptorIds: number[]
 }
 
 interface ResponseSearchStudies {
@@ -27,6 +28,16 @@ interface VariablesGetStudyByNctId {
 
 @Injectable()
 export class StudyRetrieverService {
+
+  private loadingSearchStudies = new BehaviorSubject<boolean>(false);
+  private loadingGetStudyByNctId = new BehaviorSubject<boolean>(false);
+  private loadingFilterStudies = new BehaviorSubject<boolean>(false);
+  private loadingCountStudies = new BehaviorSubject<boolean>(false);
+
+  public isLoadingSearchStudies = this.loadingSearchStudies.asObservable();
+  public isLoadingGetStudyByNctId = this.loadingGetStudyByNctId.asObservable();
+  public isLoadingFilterStudies = this.loadingFilterStudies.asObservable();
+  public isLoadingCountStudies = this.loadingCountStudies.asObservable();
 
   querySearchStudies = gql`
     query searchStudies($meshDescriptorIds: [Int]!) {
@@ -55,32 +66,58 @@ export class StudyRetrieverService {
   constructor(private apollo: Apollo) {
   }
 
+  /**
+   * Retrieve a clinical-trials study through its NCT ID.
+   * @param {string} nctId The NCT ID of the study to be retrieved.
+   */
   getTrialByNctId(nctId: string) {
-    return this.apollo.query<ResponseGetStudyByNctId, VariablesGetStudyByNctId>({
-      query: this.queryGetStudyByNctId,
-      variables: {
-        nctId: nctId
-      }
-    }).map((response) => {
-      return response.data.getStudyByNctId;
-    });
+    // Update the 'loading' observable to indicate that loading is in progress.
+    this.loadingGetStudyByNctId.next(true);
+
+    return this.apollo
+      .query<ResponseGetStudyByNctId, VariablesGetStudyByNctId>({
+        query: this.queryGetStudyByNctId,
+        variables: {
+          nctId: nctId
+        }
+      }).map((response) => {
+        // Update the 'loading' observable to indicate that loading is complete.
+        this.loadingGetStudyByNctId.next(false);
+        return response.data.getStudyByNctId;
+      });
   }
 
+  /**
+   * Search for clinical-trial studies based on an array of MeSH descriptors
+   * studies are associated with.
+   * @param {MeshDescriptorInterface[]} descriptors Array of MeSH descriptors
+   * for which the search is performed
+   */
   searchStudies(
     descriptors: MeshDescriptorInterface[],
   ) {
+    // Update the 'loading' observable to indicate that loading is in progress.
+    this.loadingSearchStudies.next(true);
 
     // Retrieve the IDs out of the provided MeSH descriptors.
-    const descriptorIds: Number[] = descriptors.map(
+    const descriptorIds: number[] = descriptors.map(
       function (d) {
         return d.descriptorId;
       }
     );
 
-    return this.apollo.query<ResponseSearchStudies, VariablesSearchStudies>({
-      query: this.querySearchStudies,
-      variables: {
-        meshDescriptorIds: descriptorIds,
+    return this.apollo
+      .query<ResponseSearchStudies, VariablesSearchStudies>({
+        query: this.querySearchStudies,
+        variables: {
+          meshDescriptorIds: descriptorIds,
+        }
+      }).map((response) => {
+        // Update the 'loading' observable to indicate that loading is complete.
+        this.loadingSearchStudies.next(false);
+        return response.data.studies.search;
+      });
+  }
       }
     }).map((response) => {
       return response.data.studies.search;
