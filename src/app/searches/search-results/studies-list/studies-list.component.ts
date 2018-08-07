201f2a6c35836e74c1330d90b9debbf3997124ca
Adamos Kyriakou <somada141@gmail.com>
2018-08-07 22:06:38 +1000

    Added a new `castMeshTermType` method to cast a mesh-term-type string to the corresponding member.

--- src/app/searches/search-results/studies-list/studies-list.component.ts
+++ src/app/searches/search-results/studies-list/studies-list.component.ts
@@ -147,7 +147,20 @@ export class StudiesListComponent implements OnInit, AfterViewInit {
   }
 
   /**
+   * Casts a fully-qualified mesh-term-type enum string coming from GraphQL,
+   * e.g. `MeshTermType.CONDITION` to the enum value as defined under the
+   * the `MeshTermType` enum.
+   * @param {string} type The fully-qualified mesh-term-type enum string.
+   * @returns {string} The corresponding `MeshTermType` value.
    */
+  castMeshTermType(type: string): MeshTermType {
+    // Split the string on `.` and keep the second part of the string with the
+    // enum member name.
+    const type_value = type.split('.')[1];
+
+    // Get corresponding `MeshTermType` member.
+    return MeshTermType[type_value];
+  }
       return 'Multiple';
     } else {
       return null;
