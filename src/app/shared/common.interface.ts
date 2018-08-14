e15871950ff4ab8046d86c0e4fc918309c32226d
Adamos Kyriakou <somada141@gmail.com>
2018-08-14 21:04:52 +1000

    Added a new interface to denote a year-range.

--- src/app/shared/common.interface.ts
+++ src/app/shared/common.interface.ts
@@ -3,6 +3,11 @@ export interface DateRange {
   dateEnd: Date
 }
 
+export interface YearRange {
+  yearBeg: number
+  yearEnd: number
+}
+
 export interface AgeRange {
   ageBeg: number
   ageEnd: number
