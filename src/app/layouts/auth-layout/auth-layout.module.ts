5a80d1aed58118ebd8535b3efcd5d0b6580acd23
Adamos Kyriakou <somada141@gmail.com>
2018-10-07 21:38:20 +1100

    Added a new `AuthLayoutModule` with its own routing.

---
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatRippleModule,
+  MatTooltipModule,
+  MatChipsModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+} from '@angular/material';
+
+import { HomeComponent } from '../../home/home.component';
+import { AuthLayoutRoutes } from './auth-layout.routing';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AuthLayoutRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
+  declarations: [
+    HomeComponent
+  ],
+  providers: [],
+  entryComponents: [],
+})
+
+export class AuthLayoutModule {}
