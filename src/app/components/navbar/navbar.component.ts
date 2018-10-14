448723d2c1f8bd143807bac4b2e7e1bc6e4e1d9f
Adamos Kyriakou <somada141@gmail.com>
2018-10-14 23:20:28 +1100

    Updated the `NavbarComponent` to use the `AuthService` and `PaymentService` to retrieve the user and customer profiles of the current user.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { ROUTES } from '../sidebar/sidebar.component';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
-import {
-  Auth0UserProfileInterface,
-  AuthService,
-} from '../../services/auth.service';
+import { AuthService } from '../../services/auth.service';
+import { BehaviorSubject, Observable } from 'rxjs/Rx';
+import { PaymentService } from '../../services/payment.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -19,13 +19,21 @@ export class NavbarComponent implements OnInit {
   private toggleButton: any;
   private sidebarVisible: boolean;
 
-  public userProfile: Auth0UserProfileInterface;
+  // Private behavior subject to indicate when the user and customer profiles
+  // are being loaded from their respective services.
+  private loadingProfiles: BehaviorSubject<boolean>
+    = new BehaviorSubject<boolean>(false);
+  // Public observable version of `loadingProfiles` that can be subscribed to
+  // by the component template.
+  public isLoadingProfiles: Observable<boolean>
+    = this.loadingProfiles.asObservable();
 
   constructor(
     location: Location,
     private element: ElementRef,
     private router: Router,
     private authService: AuthService,
+    public paymentService: PaymentService,
   ) {
     this.location = location;
     this.sidebarVisible = false;
@@ -44,13 +52,47 @@ export class NavbarComponent implements OnInit {
       }
     });
 
-    if (this.authService.userProfile) {
-      this.userProfile = this.authService.userProfile;
-    } else {
-      this.authService.getProfile((err, profile) => {
-        this.userProfile = profile;
-      });
-    }
+    // Indicate that the profiles are loading.
+    this.loadingProfiles.next(true);
+
+    // Subscribe to the `isLoadingUser` observable of the `AuthService`.
+    this.authService.isLoadingUser.subscribe(
+      (isLoadingUser: boolean) => {
+        // If the user-profile isn't loading and has not been populated then
+        // load it through the `AuthService.getUserProfile` method.
+        if (!isLoadingUser && !this.authService.userProfile) {
+          this.authService.getUserProfile();
+          // If the user-profile isn't loading and has been populated then use
+          // it to get the customer-profile through the `PaymentService`.
+        } else if (!isLoadingUser && this.authService.userProfile) {
+          // Subscribe to the `isLoadingCustomer` observable of the
+          // `PaymentService`.
+          this.paymentService.isLoadingCustomer.subscribe(
+            (isLoadingCustomer: boolean) => {
+              // If the customer-profile isn't loading has not been populated
+              // then load it through the `PaymentService.getCustomerProfile`
+              // method.
+              if (!isLoadingCustomer && !this.paymentService.customerProfile) {
+                this.paymentService
+                  .getCustomerProfile(this.authService.userProfile);
+                // If the customer-profile isn't loading and has been populated
+                // then all necessary profiles have been loaded so the
+                // `loadingProfiles` subject is updated accordingly to allow
+                // the component to render.
+              } else if (
+                !isLoadingCustomer && this.paymentService.customerProfile
+              ) {
+                this.loadingProfiles.next(false);
+              }
+            }
+          );
+        }
+      }
+    );
+
+    // Trigger the loading of the profiles by first loading the user-profile
+    // through the `AuthService.getUserProfile` method.
+    this.authService.getUserProfile();
   }
 
   sidebarOpen() {
@@ -145,6 +187,9 @@ export class NavbarComponent implements OnInit {
     return 'Dashboard';
   }
 
+  /**
+   * Logs out the current user.
+   */
   onLogout() {
     this.authService.logout()
   }
