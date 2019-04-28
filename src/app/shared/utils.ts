f22caf24d0b20b04eeab3e9b676795533ee6e885
Adamos Kyriakou <somada141@gmail.com>
2019-04-28 20:40:19 +0930

    Removed unused code.

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,42 +1,5 @@
-import {
-  MeshTermType,
-  StudyOverallStatus,
-} from '../interfaces/study.interface';
-
 import * as momentParser from 'moment-parser';
 import * as moment from 'moment';
-
-/**
- * Casts a fully-qualified overall-status enum string coming from GraphQL,
- * e.g. `OverallStatusType.COMPLETED` to the enum value as defined under the
- * the `StudyOverallStatus` enum.
- * @param status The fully-qualified overall-status enum string.
- * @returns The corresponding `StudyOverallStatus` value.
- */
-export function castOverallStatus(status: string): StudyOverallStatus {
-  // Split the string on `.` and keep the second part of the string with the
-  // enum member name.
-  const status_value = status.split('.')[1];
-
-  // Get corresponding `StudyOverallStatus` member.
-  return StudyOverallStatus[status_value];
-}
-
-/**
- * Casts a fully-qualified mesh-term-type enum string coming from GraphQL,
- * e.g. `MeshTermType.CONDITION` to the enum value as defined under the
- * the `MeshTermType` enum.
- * @param type The fully-qualified mesh-term-type enum string.
- * @returns The corresponding `MeshTermType` value.
- */
-export function castMeshTermType(type: string): MeshTermType {
-  // Split the string on `.` and keep the second part of the string with the
-  // enum member name.
-  const type_value = type.split('.')[1];
-
-  // Get corresponding `MeshTermType` member.
-  return MeshTermType[type_value];
-}
 
 /**
  * Casts an enumeration into an array of objects with and `id` property holding
