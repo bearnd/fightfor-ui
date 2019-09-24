94fca8db277b0bbe0d4a8535bd3fa03f5237348a
Adamos Kyriakou <somada141@gmail.com>
2019-09-24 17:44:05 +0930

    [ch1357]: Updated environment variable files.
    [ch1357]: Updated both the local and production environment variable files to define the host from which the website is running, the HTTP protocol, and the app version and used these variables to assemble the relevant `environment` variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,34 @@
+const protocol = 'http';
+const host = window.location.host;
+const hostApiGateway = '192.168.0.12';
 
 export const environment = {
   production: false,
+  protocol: protocol,
+  version: '0.6.0',
   apiGateway: {
-    domain: '192.168.0.12',
+    domain: hostApiGateway,
   },
   graphql: {
-    uri: 'http://192.168.0.12/fightfor-graphql/graphql',
+    uri: protocol + '://' + hostApiGateway + '/fightfor-graphql/graphql',
   },
   mapbox: {
-    uri: 'http://192.168.0.12/mapbox',
+    uri: protocol + '://' + hostApiGateway + '/mapbox',
   },
   auth0: {
     clientID: 'owOhMvKuo0cEjsuObyBfxk4y6F0jP8P1',
     domain: 'bearnd.auth0.com',
     responseType: 'token id_token',
-    redirectUri: 'http://localhost:4200/callback',
+    redirectUri: protocol + '://' + host + '/callback',
     scope: 'openid profile email user_metadata app_metadata',
     audience: 'fightfor-backend-dev',
   },
   braintreeGateway: {
-    uri: 'http://192.168.0.12/braintree-gateway',
+    uri: protocol + '://' + hostApiGateway + '/braintree-gateway',
     planId: 'fightfor_premium_monthly'
   },
   sentry: {
     dsn: 'https://8471eee156ee4de8bd5086f92a3a4105@sentry.io/1438884',
+    environment: 'development'
   },
 };
