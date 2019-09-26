c1121b59e68e0a231597a002baf5374f93fd343b
Adamos Kyriakou <somada141@gmail.com>
2019-09-26 21:22:04 +0930

    [ch945]: Replaced Sentry dialog with custom one.
    [ch945]: Updated the `SentryErrorHandler` and replaced the built-in Sentry feedback dialog with a custom SweetAlert dialog using the same form-data POST method.

--- src/app/utils/sentryerrorhandler.ts
+++ src/app/utils/sentryerrorhandler.ts
@@ -3,8 +3,10 @@ import { environment } from '../../environments/environment';
 
 import * as Sentry from '@sentry/browser';
 import * as _ from 'lodash';
+import swal from 'sweetalert2';
 
 import { AuthService } from '../services/auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 Sentry.init({
@@ -18,12 +20,63 @@ Sentry.init({
 export class SentryErrorHandler implements ErrorHandler {
 
   private authService: AuthService;
+  private httpClient: HttpClient;
 
   constructor(private injector: Injector) {
     setTimeout(
       () => {
         this.authService = injector.get(AuthService);
+        this.httpClient = injector.get(HttpClient);
       }
+    );
+  }
+
+  /**
+   * Assembles the Sentry feedback URL based on the dialog parameters.
+   * @param eventId The ID of the Sentry event created over a new error.
+   * @param name The name of the user sending feedback.
+   * @param email The email of the user sending feedback.
+   * @param title The title used in the feedback dialog.
+   * @param subtitle The subtitle used in the feedback dialog.
+   * @param labelComments The user feedback provided via the dialog.
+   */
+  assembleSentryFeedbackUrl(
+    eventId: string,
+    name: string | null,
+    email: string | null,
+    title: string,
+    subtitle: string,
+    labelComments: string | null,
+  ): string {
+    return encodeURI(
+      'https://sentry.io/api/embed/error-page/' +
+      '?dsn=' + environment.sentry.dsn +
+      '&eventId=' + eventId +
+      '&dsn=' + environment.sentry.dsn +
+      '&name=' + name +
+      '&email=' + email +
+      '&title=' + title +
+      '&subtitle=' + subtitle +
+      '&labelComments=' + labelComments
+    );
+  }
+
+  /**
+   * Assembles the Sentry feedback form-data based on the dialog parameters.
+   * @param name The name of the user sending feedback.
+   * @param email The email of the user sending feedback.
+   * @param labelComments The user feedback provided via the dialog.
+   */
+  assembleSentryFeedbackFormData(
+    name: string | null,
+    email: string | null,
+    labelComments: string | null,
+  ): string {
+    return encodeURI(
+      'name=' + name +
+      '&email=' + email +
+      '&comments=' + labelComments +
+      '&='
     );
   }
 
@@ -34,6 +87,7 @@ export class SentryErrorHandler implements ErrorHandler {
    * @param error The error that occurred.
    */
   handleError(error: any) {
+    // Capture the error and create a new Sentry event.
     const eventId = Sentry
       .captureException(error.originalError || error);
 
@@ -42,17 +96,52 @@ export class SentryErrorHandler implements ErrorHandler {
       console.error(error);
     }
 
-    Sentry.showReportDialog({
-      eventId: eventId,
-      dsn: environment.sentry.dsn,
-      user: {
-        name: _.has(this.authService, ['userProfile', 'name']) ? this.authService.userProfile.name : null,
-        email: _.has(this.authService, ['userProfile', 'email']) ? this.authService.userProfile.email : null,
-      },
-      title: 'Oops seems you encountered an error!',
-      subtitle: 'Feel free to leave some feedback as to what you were ' +
-                'doing or to just yell at us.',
-      labelComments: 'Your feedback'
+    // Define the components of the feedback dialog.
+    const title = 'Oops seems you encountered an error!';
+    const subtitle = 'Feel free to leave some feedback as to what you were ' +
+      'doing or to just yell at us.';
+    // Use the name and the email of the currently logged in user to use a
+    // default if unavailable.
+    const name = _.has(
+      this.authService, ['userProfile', 'name']
+    ) ? this.authService.userProfile.name : 'support@fightfor.app';
+    const email = _.has(
+      this.authService, ['userProfile', 'email']
+    ) ? this.authService.userProfile.email : 'support@fightfor.app';
+    const footer = '<p>Email <a href="mailto:support@fightfor.app">' +
+      'support@fightfor.app</a></p>';
+
+    // Assemble request headers.
+    const headers: HttpHeaders = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
     });
+
+    // Show a SweetAlert dialog prompting the user to provide feedback over the
+    // encountered error.
+    swal({
+      title: title,
+      text: subtitle,
+      footer: footer,
+      showCancelButton: true,
+      showConfirmButton: true,
+      buttonsStyling: false,
+      confirmButtonClass: 'btn btn-rose',
+      confirmButtonText: 'Submit',
+      cancelButtonClass: 'btn btn-rose',
+      type: 'warning',
+      allowOutsideClick: true,
+      input: 'textarea',
+      inputPlaceholder: 'Type your message here...',
+    }).then(result => {
+      if (result.value) {
+        this.httpClient.post(
+          this.assembleSentryFeedbackUrl(
+            eventId, name, email, title, subtitle, result.value,
+          ),
+          this.assembleSentryFeedbackFormData(name, email, result.value),
+          {headers: headers},
+        ).subscribe();
+      }
+    }).catch(swal.noop);
   }
 }
