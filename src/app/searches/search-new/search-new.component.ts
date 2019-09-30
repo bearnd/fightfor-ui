b868958de5bcb644d828a9f5a5ff16f4f1552b39
Adamos Kyriakou <somada141@gmail.com>
2019-10-01 07:07:12 +0930

    [ch1507]: Moved `SearchNewComponent` to behavior subject.
    [ch1507]: Updated the `SearchNewComponent` class to use the `searchesLatest` behavior subject instead of the `isCreatingNewSearch` observable.

--- src/app/searches/search-new/search-new.component.ts
+++ src/app/searches/search-new/search-new.component.ts
@@ -30,6 +30,7 @@ import {
 } from '../../services/study-stats-retriever.service';
 import { UserConfigService } from '../../services/user-config.service';
 import { AuthService } from '../../services/auth.service';
+import { SearchInterface } from '../../interfaces/user-config.interface';
 
 
 @Component({
@@ -46,6 +47,8 @@ export class SearchNewComponent implements OnInit, OnDestroy {
 
   // Subscription to the observable returned by `getMeshDescriptorsBySynonym`.
   subscriptionDescriptors: Subscription;
+  // Subscription to the `userConfigService.searchesLatest` behavior-subject.
+  subscriptionSearchesLatest: Subscription = null;
 
   // `FormGroup` to encompass the form controls.
   form: FormGroup;
@@ -155,13 +158,14 @@ export class SearchNewComponent implements OnInit, OnDestroy {
       }
     );
 
-    // Subscribe to the `userConfigService.isCreatingNewSearch` observable.
-    this.userConfigService.isCreatingNewSearch.subscribe(
-      (isCreating: boolean) => {
-        // If the new search UUID has been set (which happens in the `onSubmit`
-        // method) and the new search is finished being created then navigate
-        // the result summary page for this new search.
-        if (!isCreating && this.newSearchUuid !== null) {
+    // Subscribe to the `userConfigService.searchesLatest` behavior subject. If
+    // the new search UUID has been set (which happens in the `onSubmit` method)
+    // and the new search is finished being created then navigate to the result
+    // summary page for this new search.
+    this.subscriptionSearchesLatest = this.userConfigService
+      .searchesLatest.subscribe(
+      (searches: SearchInterface[]) => {
+        if (searches && this.newSearchUuid !== null) {
           if (this.userConfigService.getUserSearch(this.newSearchUuid)) {
             const result = this.router.navigate(
               ['/app', 'searches', this.newSearchUuid]
@@ -263,6 +267,9 @@ export class SearchNewComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     if (this.subscriptionDescriptors) {
       this.subscriptionDescriptors.unsubscribe();
+    }
+    if (this.subscriptionSearchesLatest) {
+      this.subscriptionSearchesLatest.unsubscribe();
     }
   }
 }
