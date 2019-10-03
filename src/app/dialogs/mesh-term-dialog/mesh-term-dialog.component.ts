84e14b3071a5d7d471772d0cd755c3639e809d1c
Adamos Kyriakou <somada141@gmail.com>
2019-10-03 18:32:06 +0930

    [ch1520]: Moved `getIconClass` function.
    [ch1520]: Updated the `MeshTermDialogComponent` class and moved the `getIconClass` method to a shared function under a new file.

--- src/app/dialogs/mesh-term-dialog/mesh-term-dialog.component.ts
+++ src/app/dialogs/mesh-term-dialog/mesh-term-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 import gql from 'graphql-tag';
 
@@ -12,7 +13,7 @@ import {
 import {
   MeshDescriptorRetrieverService
 } from '../../services/mesh-descriptor-retriever.service';
-import { Router } from '@angular/router';
+import { getDescriptorIconClass } from '../../utils/mesh-term.utils';
 
 
 interface SourceNameUrlInterface {
@@ -175,80 +176,11 @@ export class MeshTermDialogComponent implements OnInit {
   ngOnInit() {}
 
   /**
-   * Returns an object specifying the icon and name of the displayed
-   * descriptor's category.
+   * Returns an object specifying the icon and name of the currently displayed
+   * MeSH descriptor.
    */
-  getIconClass(): {iconClass: string, category: string} {
-
-    const treeNumber: string = this.descriptor.treeNumbers[0].treeNumber;
-
-    if (treeNumber.startsWith('A')) {
-      return {iconClass: 'fa-child', category: 'Anatomy'};
-    } else if (treeNumber.startsWith('B')) {
-      return {iconClass: 'fa-frog', category: 'Organisms'};
-    } else if (treeNumber.startsWith('C')) {
-      return {iconClass: 'fa-allergies', category: 'Diseases'};
-    } else if (treeNumber.startsWith('D')) {
-      return {iconClass: 'fa-capsules', category: 'Chemicals and Drugs'};
-    } else if (treeNumber.startsWith('E')) {
-      return {
-        iconClass: 'fa-chart-bar',
-        category: 'Analytical, Diagnostic and Therapeutic Techniques, and ' +
-          'Equipment ',
-      };
-    } else if (treeNumber.startsWith('F')) {
-      return {iconClass: 'fa-brain', category: 'Psychiatry and Psychology'};
-    } else if (treeNumber.startsWith('G')) {
-      return {
-        iconClass: 'fa-book-medical',
-        category: 'Phenomena and Processes',
-      };
-    } else if (treeNumber.startsWith('H')) {
-      return {
-        iconClass: 'fa-briefcase-medical',
-        category: 'Disciplines and Occupations',
-      };
-    } else if (treeNumber.startsWith('I')) {
-      return {
-        iconClass: 'fa-user-graduate',
-        category: 'Anthropology, Education, Sociology, and Social Phenomena',
-      };
-    } else if (treeNumber.startsWith('J')) {
-      return {
-        iconClass: 'fa-laptop-code',
-        category: 'Technology, Industry, and Agriculture',
-      };
-    } else if (treeNumber.startsWith('K')) {
-      return {
-        iconClass: 'fa-book-reader',
-        category: 'Humanities',
-      };
-    } else if (treeNumber.startsWith('L')) {
-      return {
-        iconClass: 'fa-database',
-        category: 'Information Science',
-      };
-    } else if (treeNumber.startsWith('M')) {
-      return {
-        iconClass: 'fa-user-tag',
-        category: 'Named Groups',
-      };
-    } else if (treeNumber.startsWith('N')) {
-      return {
-        iconClass: 'fa-hand-holding-heart',
-        category: 'Health Care',
-      };
-    } else if (treeNumber.startsWith('V')) {
-      return {
-        iconClass: 'fa-tags',
-        category: 'Publication Characteristics',
-      };
-    } else if (treeNumber.startsWith('Z')) {
-      return {
-        iconClass: 'fa-map-marker-alt',
-        category: 'Geographicals',
-      };
-    }
+  getIconClass() {
+    return getDescriptorIconClass(this.descriptor.treeNumbers[0].treeNumber);
   }
 
   /**
