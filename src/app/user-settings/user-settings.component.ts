b2ca6cb2f4162bc145a1fadd0339311a3899d704
Adamos Kyriakou <somada141@gmail.com>
2019-10-15 20:32:24 +1030

    [ch987]: Added `getUserIdentityProvider` method.
    [ch987]: Updated the `UserSettingsComponent` class and added a new `getUserIdentityProvider` method to retrieve a string denoting the identity provider of the user included in the template.

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -9,9 +9,32 @@ import {AuthService} from '../services/auth.service';
 export class UserSettingsComponent implements OnInit {
 
   constructor(
-    public authService: AuthService
-  ) {}
+    public authService: AuthService,
+    private clubhouseService: ClubhouseService,
+  ) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
+  /**
+   * Retrieves a string signifying the identity provider with which the user
+   * signed up for this account.
+   * @return A string signifying the identity provider with which the user
+   * signed up for this account
+   */
+  getUserIdentityProvider(): string {
+    let provider: string = null;
+    const identityProvider = this.authService.userProfile.identityProvider;
+
+    if (identityProvider === UserIdentityProvider.FACEBOOK) {
+      provider = 'Facebook';
+    } else if (identityProvider === UserIdentityProvider.GOOGLE) {
+      provider = 'Google';
+    } else {
+      provider = 'Email';
+    }
+
+    return provider;
+  }
 }
