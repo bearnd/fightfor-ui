bedf9f1b09f79c409bb097ea2cdfb117e16454ed
Adamos Kyriakou <somada141@gmail.com>
2019-10-15 20:36:27 +1030

    [ch987]: Added `onRequestRetrieval` method.
    [ch987]: Updated the `UserSettingsComponent` class and added a new `onRequestRetrieval` method to raise a Clubhouse story requesting a GDPR data retrieval which was exposed in the template.

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../services/auth.service';
+import swal from 'sweetalert2';
+
+import { AuthService, UserIdentityProvider } from '../services/auth.service';
+import {
+  ClubhousePostStoryResponse,
+  ClubhouseService
+} from '../services/clubhouse.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-user-settings',
@@ -7,6 +14,9 @@ import {AuthService} from '../services/auth.service';
   styleUrls: ['./user-settings.component.scss']
 })
 export class UserSettingsComponent implements OnInit {
+
+  footer = '<p>Email <a href="mailto:support@fightfor.app">' +
+    'support@fightfor.app</a></p>';
 
   constructor(
     public authService: AuthService,
@@ -37,4 +47,54 @@ export class UserSettingsComponent implements OnInit {
 
     return provider;
   }
+
+  /**
+   * Shows an alert explaining the process of data-retrieval to the user and
+   * allows them to change the email address to which the data will be sent.
+   */
+  onRequestRetrieval() {
+    swal({
+      title: 'Request your stored personal data',
+      text: 'Per GDPR you are entitled to retrieving a copy of your personal ' +
+        'data this service is storing and processing. This is a manual process' +
+        'that may take up to 30 days following the request. Your collected data' +
+        'will be sent to your registered email address shown below unless you ' +
+        'wish to use a different address.',
+      footer: this.footer,
+      showCancelButton: true,
+      showConfirmButton: true,
+      buttonsStyling: false,
+      confirmButtonClass: 'btn btn-primary btn-dark-blue',
+      confirmButtonText: 'Request',
+      cancelButtonClass: 'btn btn-rose',
+      type: 'info',
+      allowOutsideClick: true,
+      input: 'text',
+      inputPlaceholder: this.authService.userProfile.email,
+    }).then(result => {
+      if (!result.dismiss) {
+        const userEmail = result.value
+          ? result.value
+          : this.authService.userProfile.email;
+        this.clubhouseService.createStory({
+          name: 'GDPR Data Request: ' + userEmail,
+          description: 'User ' + userEmail + ' requested a copy of their ' +
+            'stored data.',
+          project_id: environment.clubhouse.supportProjectId,
+          story_type: 'chore',
+        }).subscribe(
+          (_: ClubhousePostStoryResponse) => {
+            swal({
+              title: 'Submitted',
+              text: 'Your request has been submitted.',
+              buttonsStyling: false,
+              confirmButtonClass: 'btn btn-primary btn-dark-blue',
+              type: 'success'
+            }).catch(swal.noop);
+          }
+        );
+      }
+    }).catch(swal.noop);
+  }
+
 }
