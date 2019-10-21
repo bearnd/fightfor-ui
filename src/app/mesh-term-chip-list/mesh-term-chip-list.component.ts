e6218e5b85ed78f8ccf5bfbba45fc3e29847582e
Adamos Kyriakou <somada141@gmail.com>
2019-10-21 22:29:47 +1030

    Replaced eye icon on MeSh chips.
    Updated the `MeshTermChipComponent` and `MeshTermChipListComponent` and replaced the eye icon on the chips with an `i` icon.

--- src/app/mesh-term-chip-list/mesh-term-chip-list.component.ts
+++ src/app/mesh-term-chip-list/mesh-term-chip-list.component.ts
@@ -10,7 +10,7 @@ import { DescriptorInterface } from '../interfaces/descriptor.interface';
 export class MeshTermChipListComponent {
 
   @Input() descriptors: DescriptorInterface[];
-  @Input() trailingIcon = 'remove_red_eye';
+  @Input() trailingIcon = 'info_outline';
   @Input() removeIcon = null;
   @Input() selectable = false;
   @Input() removable = false;
